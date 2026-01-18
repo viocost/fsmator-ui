d5d380f64a2467281a5fd2967abd35302bc7420d
Konstantin Y. Rybakov <viocost@gmail.com>
2026-01-18 13:30:30 +0200

    mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/fsmator-ui/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/fsmator-ui/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+}))
